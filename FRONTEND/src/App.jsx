04513d0e64781edfb3dc1f818ff8e45f5a086b0a
Menhaz <menhazul12345@gmail.com>
2025-10-18 03:09:35 +0530

    PROJECT SETUP

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'//ADD THIS IN EVERY FOLDER TO USE TAILWIND 
+//npm run dev to run the project
+//npm i for installing all dependencies
+
+
+function App() {
+
+
+  return (
+    <div class=" from-purple-100  to-purple-800 bg-gradient-to-r h-screen flex justify-center items-center">
+      <h1 class="text-4xl font-medium text-center bg-gradient-to-r from-purple-900 via-pink-900 to-purple-200 bg-clip-text text-transparent">HII ZARA ASHA KRTE HN KI APKO KOI PARESHANI NHI HORHI HO </h1>
+    </div>
+  )
+}
+
+export default App
